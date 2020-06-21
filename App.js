72ad82d66c1a094e29dc961212180168ff7490c8
Rishit123344 <bramarasunku@gmail.com>
2020-06-21 12:40:48 +0530

    Added text boxes

--- App.js
+++ App.js
@@ -1,27 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput,TouchableOpacity,Image } from 'react-native';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import {createAppContainer} from 'react-navigation';
 import ReadScreen from './Screens/ReadScreen';
 import WriteScreen from './Screens/WriteScreen';
 
 export default class app extends React.Component {
-  render(){
-    return (
-    <AppContainer/>
-  );
-}
+ render(){
+   return(
+<AppContainer></AppContainer>
+   );
+ }
 }
 const TabNavigator = createBottomTabNavigator({
   Read:{screen:ReadScreen},
   Write:{screen:WriteScreen}
+},
+{
+  defaultNavigationOptions: ({navigation})=>({
+    tabBarIcon:({})=>{
+      const routeName = navigation.state.routeName
+      if(routeName === 'Read'){
+        return(
+          <Image source = {require('./assets/leather-book-preview.png')} style={{width:40,height:40}}/>
+        )
+      }
+      else  if(routeName === 'Write'){
+        return(
+          <Image source = {require('./assets/th.jpg')} style={{width:40,height:40}}/>
+        )
+      }
+    }
+  })
 })
 const AppContainer = createAppContainer(TabNavigator)
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+  
+
+
--- Screens/WriteScreen.js
+++ Screens/WriteScreen.js
@@ -1,12 +1,61 @@
 import React from 'react'
-import {Text,View} from 'react-native'
+import {Text,View,StyleSheet,TextInput,TouchableOpacity} from 'react-native'
 
 export default class WriteScreen extends React.Component{
-    render(){
-return(
-<View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-<Text>Write Stories</Text>
-</View>
-);
+    constructor(){
+        super();
+        this.state={
+displayText:''
+
+        }
     }
-}
+    render() {
+        return (
+          <View style={styles.container}>
+          <TextInput onChangeText={(text)=>{this.setState({
+            text:text
+          })}} value={this.state.text} style={styles.inputbox}multiline numberOfLines={100000000000000}/>
+          <TouchableOpacity style={styles.goButton}onPress={()=>{
+            this.setState({
+              displayText:this.state.text
+            })
+          }}><Text style={styles.buttontext}>Go</Text></TouchableOpacity>
+          <Text style={styles.displayText}>{this.state.displayText}</Text>
+          </View>
+         
+        );
+      }
+    }
+    const styles = StyleSheet.create({
+        inputbox: {
+          marginTop: 100,
+          width: 300,
+          alignSelf: 'center',
+          height: 40,
+          textAlign: 'center',
+          borderWidth: 4,
+        },
+        goButton: {
+          width: '-50%',
+          height: 55,
+          alignSelf: 'center',
+          padding: 10,
+          margin: 10,
+        },
+        buttontext: {
+          textAlign: 'center',
+          fontSize: 30,
+          fontWeight: 'bold'
+        },
+        displayText: {
+          textAlign: 'center',
+          fontSize: 30,
+        },
+        container: {
+          flex: 1,
+          backgroundColor: '#fff',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }
+        });
+       
